55a8494cfa66da21fb8eaa7d26c772424ac85bfb
gurongjing <2698090189@qq.com>
2017-01-05 19:35:41 +0800

    modify

--- core/spider01.py
+++ core/spider01.py
@@ -9,7 +9,7 @@ def getHtml(url):
     return html
 
 # ------ getHtml()内输入任意帖子的URL ------
-html = getHtml("http://tieba.baidu.com/p/3205263090")
+html = getHtml("http://tieba.baidu.com/p/4911971389")
 # ------ 修改html对象内的字符编码为UTF-8 ------
 html = html.decode('UTF-8')
 
@@ -31,10 +31,10 @@ def getNextUrl(html):
 
 
 imgList = getImg(html)
-imgName = 0
+imgName = 29
 for imgPath in imgList:
     # ------ 这里最好使用异常处理及多线程编程方式 ------
-    f = open("e:/pic/"+str(imgName)+".jpg", 'wb')
+    f = open("e:/spider/pic/"+str(imgName)+".jpg", 'wb')
     f.write((urllib.request.urlopen(imgPath)).read())
     f.close()
     imgName += 1
